27fc2f16fb06173b5daf35d63b9ca94dceccc4dc
Pradip Chand <bhupendrachand749@gmail.com>
2025-05-30 08:43:45 +0545

    database connect

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,9 @@ import app from "./src/app";
 import { config } from "dotenv";
 config()
 
+
+
+import  "./src/database/connection";
 function startServer(){
     const port = process.env.PORT 
     app.listen(port,function(){
