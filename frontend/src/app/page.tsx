f3f5dea456b18366a233fd9681ff6fe9506775ae
bhupendra chand <bhupendrachand749@gmail.com>
2025-07-10 20:49:16 +0545

    slice part of login institute teacher register

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,7 @@
-import { setName } from "@/lib/store/user.slice";
-
-import { useDispatch } from "react-redux";
 
 
 export default function Home() {
-  let name = "romeo"
-  const dispatch = useDispatch()
-  dispatch(setName(name))
+  
   return (
    <h1>haha hehe huhu</h1>
   );
---
+++ frontend/src/lib/types/type.ts
@@ -0,0 +1,6 @@
+//  import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+export enum Status{
+    SUCCESS = "success", 
+    LOADING = "loading", 
+    ERROR = "error"
+}
