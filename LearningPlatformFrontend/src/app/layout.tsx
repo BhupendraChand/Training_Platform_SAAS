07139b36b7a698070050269452e818a7b4337ce9
bhupendra chand <bhupendrachand749@gmail.com>
2025-08-17 20:24:37 +0545

    frontend institute

---
+++ LearningPlatformFrontend/src/app/layout.tsx
@@ -0,0 +1,43 @@
+"use client"
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Provider } from "react-redux";
+import store from "@/lib/store/store";
+import Navbar from "@/lib/components/navbar/Navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+// export const metadata: Metadata = {
+//   title: "Nepal best IT learning platform",
+//   description: "Nepal best IT learning platform",
+// };
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+      <Provider store={store}>
+      <Navbar />
+        {children}
+     
+        </Provider>
+      </body>
+    </html>
+  );
+}
